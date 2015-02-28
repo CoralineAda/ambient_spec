f803bdb9ca987bde2f284a4c5f4489c93ecf57d9
WendyBeth <wendybeth010@gmail.com>
2015-02-27 22:42:27 -0500

    check if RSpec is defined before calling AmbientSpec::Configure.new.rspec

--- lib/ambient_spec.rb
+++ lib/ambient_spec.rb
@@ -1,10 +1,23 @@
-require "rspec/core"
+if defined?(Rspec)
+  require "rspec/core"
+else
+  require "minitest/unit"
+end
+
 require "ambient_spec/version"
 require "ambient_spec/player"
 require "ambient_spec/configure"
 
 module AmbientSpec
-  unless ENV['AMBIENT_SPEC_DISABLED']
-    AmbientSpec::Configure.new.rspec
+  unless ENV['AMBIENT_SPEC_DISABLE']
+    if defined?(RSpec)
+      AmbientSpec::Configure.new.rspec
+    end
   end
+
+  # IDEAL
+  # else
+  #   AmbientSpec::Configure.new.minitest
+  # end
+
 end
