05f85f41889f43c09b9eee036a859d6adb8ffdca
Coraline Ada Ehmke <coraline@idolhands.com>
2019-05-12 16:33:49 -0500

    Minor cleanup

--- lib/ambient_spec/player.rb
+++ lib/ambient_spec/player.rb
@@ -6,16 +6,17 @@ module AmbientSpec
 
     FIFTHS = %w{c g d a e b f}.freeze
     PATCHES = %w{breathy dark ghost harp piano plasma swirl waves}.freeze
+    PATH_TO_FILES = File.join(File.dirname(File.expand_path(__FILE__)), 'sounds').freeze
 
     def sing(status=:pass)
       if status == :pass
         return unless should_play?
         folder = PATCHES.sample
         filename = FIFTHS.sample
-        system("afplay #{path_to_files}/#{folder}/#{filename}.wav &>/dev/null &")
+        system("afplay #{PATH_TO_FILES}/#{folder}/#{filename}.wav &>/dev/null &")
         increment_timer
       else
-        system("afplay #{path_to_files}/failure/failure.wav &>/dev/null &")
+        system("afplay #{PATH_TO_FILES}/failure/failure.wav &>/dev/null &")
       end
     end
 
@@ -27,10 +28,6 @@ module AmbientSpec
 
     def increment_timer
       @last_played_at = Time.now
-    end
-
-    def path_to_files
-      @path_to_files ||= File.join(File.dirname(File.expand_path(__FILE__)), 'sounds')
     end
 
   end
