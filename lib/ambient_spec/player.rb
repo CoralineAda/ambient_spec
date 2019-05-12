d87bd434c4e07545939480b44cbeb5863c3a4b4d
Coraline Ada Ehmke <coraline@idolhands.com>
2019-05-12 16:37:51 -0500

    Throttle sounds to accommodate fast specs

--- lib/ambient_spec/player.rb
+++ lib/ambient_spec/player.rb
@@ -4,30 +4,30 @@ module AmbientSpec
 
   class Player
 
-    attr_accessor :counter
-
-    def fifths
-      ["c", "g", "d", "a", "e", "b", "f"]
-    end
+    FIFTHS = %w{c g d a e b f}.freeze
+    PATCHES = %w{breathy dark ghost harp piano plasma swirl waves}.freeze
+    PATH_TO_FILES = File.join(File.dirname(File.expand_path(__FILE__)), 'sounds').freeze
 
     def sing(status=:pass)
-      increment_counter
-#      return unless counter % 3 == 1 || status == :fail
       if status == :pass
-        folder = %w{breathy dark ghost harp piano plasma swirl waves}.sample
-        filename = fifths.sample
-        system("afplay #{path_to_files}/#{folder}/#{filename}.wav &>/dev/null &")
+        return unless should_play?
+        folder = PATCHES.sample
+        filename = FIFTHS.sample
+        system("afplay #{PATH_TO_FILES}/#{folder}/#{filename}.wav &>/dev/null &")
+        increment_timer
       else
-        system("afplay #{path_to_files}/failure/failure.wav &>/dev/null &")
+        system("afplay #{PATH_TO_FILES}/failure/failure.wav &>/dev/null &")
       end
     end
 
-    def increment_counter
-      self.counter ||= 0; self.counter += 1
+    private
+
+    def should_play?
+      @last_played_at.nil? || Time.now - @last_played_at > 1
     end
 
-    def path_to_files
-      @path_to_files ||= File.join(File.dirname(File.expand_path(__FILE__)), 'sounds')
+    def increment_timer
+      @last_played_at = Time.now
     end
 
   end
